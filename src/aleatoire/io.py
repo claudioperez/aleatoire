39416cdc0fc6f4a54b5df00691feb3eeb1bde979
claudioperez <50180406+claudioperez@users.noreply.github.com>
2020-12-15 12:29:36 -0800

    Add convenience functions for Weibull distribution.

--- src/aleatoire/io.py
+++ src/aleatoire/io.py
@@ -1,9 +1,32 @@
 import json
 import numpy as np
 import scipy.stats
+import scipy.special
+from scipy.special import gamma as gamma_func
 
+def _weibull_mean(shape,scale):
+    r"""
+    \mathrm{E}(X)=\lambda \Gamma\left(1+\frac{1}{k}\right)
+    """
+    return scale*gamma_func(1+1/shape)
 
+def _weibull_variance(shape, scale):
+    r"""
+    given $\lambda$ (`scale`) and $k$ (`shape`), the variance is computed as:
+    \operatorname{var}(X)=\lambda^{2}\left[\Gamma\left(1+\frac{2}{k}\right)-\left(\Gamma\left(1+\frac{1}{k}\right)\right)^{2}\right]
+    """
+    return scale**2 * (gamma_func(1+2/shape)-gamma_func(1+1/shape)**2)
 
+def weibull_shape(mean,stdDev=None,cov=None):
+    if stdDev is None:
+        stdDev = cov*mean
+    return (stdDev/mean)**(-1.086)
+
+def weibull_scale(mean,stdDev=None,cov=None):
+    if stdDev is None:
+        stdDev = cov*mean
+    scale =  lambda shape: mean/(gamma_func(1+1/shape))
+    return scale(weibull_shape(mean, stdDev))
 
 def normal(mean, stdDev,**kwds):
 
